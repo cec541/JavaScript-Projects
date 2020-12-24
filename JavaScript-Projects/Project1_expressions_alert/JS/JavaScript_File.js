d4f58e50931eb310a7eb33035bbc4fea61e6309e
cec541 <75457819+cec541@users.noreply.github.com>
2020-12-24 00:15:44 -0800

    Add files via upload

---
+++ JavaScript-Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,7 @@
+window.alert("Hello, world!"); //alert when webpage is opened
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue"); //Making the font blue
+document.write(blues);
+document.write(3+3); //Writing a fuction
+var B = "concatenated" + "String"
+document.write(B)
