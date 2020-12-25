8fe0efd18b7fd67ae0e232263c8c2226d58ea4cf
cec541 <75457819+cec541@users.noreply.github.com>
2020-12-24 21:22:59 -0800

    Add files via upload

---
+++ Project3_math_operators/JS/main.js
@@ -0,0 +1,30 @@
+function addition_Function() {
+    var addition = 2 + 2; 
+    document.getElementById("Math") .innerHTML = "2 + 2 = " + addition; //adding the addition function
+}
+function subtraction_Function() {
+    var subtraction = 5 - 2; 
+    document.getElementById("Math1") .innerHTML = "5 - 2 = " + subtraction; //subtraction function added
+}
+function multiplication() {
+    var simple_Math = 6 * 8; 
+    document.getElementById("Math2") .innerHTML = "6 * 8 = " + simple_Math; //multiplication function added
+}
+function division() {
+    var simple_Math = 48 / 6; 
+    document.getElementById("Math3") .innerHTML = "48 / 6 = " +  simple_Math; //division function added 
+}
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5; 
+    document.getElementById("Math4") .innerHTML = "1 plus 2, multiplied by 10,divided in half and the subtracted by 5 equals " +  simple_Math; //multipy math functions added
+}
+
+{var X = 5;
+X++;
+document.write(X);
+}
+
+{var T = 5.25;
+T--;
+document.write(T);
+}
