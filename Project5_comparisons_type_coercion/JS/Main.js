d9554cfe39706250048516594d3e6b9e723e51bc
cec541 <75457819+cec541@users.noreply.github.com>
2020-12-26 03:35:44 -0800

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/Main.js
@@ -0,0 +1,23 @@
+document.write(typeof "Word");
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write("10" + 5);
+
+document.write(10==11);
+
+console.log(2+2);
+
+X = 82;
+Y = "82";
+document.write(X === Y);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
