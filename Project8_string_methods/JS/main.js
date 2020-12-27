14cf32c1d72b192154840e39702f2086be79b373
cec541 <75457819+cec541@users.noreply.github.com>
2020-12-26 20:46:57 -0800

    Add files via upload

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,35 @@
+function full_Sentence() {
+    var part_1= "I have ";
+    var part_2= "made this ";
+    var part_3= "into a complete ";
+    var part_4= "sentence";
+    var whole_sentence = part_1.concat(part_2, part_3, part_4);
+    document.getElementById("Concatenate").innerHTML = whole_sentence; //function to put whole thing together
+}
+
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
+function myFunction() {
+    var str = "i'm so excited!";
+    var res = str.toUpperCase();
+    document.getElementById("Capital").innerHTML = res; //captilize whole thing
+  }
+
+  function string_Method() {
+      var X =182;
+      document.getElementById("Numbers_to_string").innerHTML = X.toString();
+  }
+
+  function precision_Method() {
+      var X = 12938.3012987376112;
+      document.getElementById("Precision") .innerHTML = X.toPrecision(10);
+  }
+  function fixed_Function() {
+    var num = 5.5;
+    var n = num.toFixed(2);
+    document.getElementById("Rounding").innerHTML = n;
+  }
